a6ab37a4025f5febbc1997f46f038e5e667e5f8b
Yryskeldi <yryskeldi.kurmanbekov2@gmail.com>
2023-10-13 14:38:56 -0400

    fix(restclient): added timeouts for the resttemplate

--- src/main/java/com/synpulse8/challenge/AppConfig.java
+++ src/main/java/com/synpulse8/challenge/AppConfig.java
@@ -1,6 +1,9 @@
 package com.synpulse8.challenge;
 
+import java.time.Duration;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +14,10 @@ import org.springframework.web.client.RestTemplate;
 @EnableRetry
 public class AppConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofMillis(5 * 1000))
+                .setReadTimeout(Duration.ofMillis(10 * 1000))
+                .build();
     }
 }
